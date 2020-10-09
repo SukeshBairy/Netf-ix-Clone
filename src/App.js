99d111a6ba8e8ee4eba5ea3e16f4c99f42cdd180
surya-config <a1b2c3d4config@gmail.com>
2020-10-09 09:36:24 +0530

    Netflix Clone

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import requests from "./requests";
+import Row from "./Row.js";
+import Banner from "./Banner.js";
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
+
+// f81df41f206cfcf1be3fbf7649e542cd
